d3f6b67c9d54f52659cf0ce73127ca7fa23b9858
Muhammad Salahuddin <msalahuddin8890@gmail.com>
2020-04-22 19:26:37 +0500

    alignment set

--- app.js
+++ app.js
@@ -1,47 +1,49 @@
-var createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+var createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-const indexRouter = require('./routes/index');
-const helmet = require('helmet');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const indexRouter = require("./routes/index");
+const helmet = require("helmet");
+const mongoose = require("mongoose");
+const cors = require("cors");
 //***** ///// *****//
 var app = express();
-// mongoose.connect('mongodb+srv://abdul:a!123456@cluster0-kfpwx.mongodb.net/test?retryWrites=true&w=majority')
-// mongoose.connect('mongodb+srv://abdulbhai:W123456@cluster0-71cfj.mongodb.net/test?retryWrites=true&w=majority')
-mongoose.connect('mongodb://localhost/SaloonApp')
-.then(()=> console.log('connected to Saloon APP...'))
-.catch((err)=> console.error('Could not connect to database...', err));
+
+mongoose
+  .connect(
+    "mongodb+srv://sud:12345sud@cluster0-dhwk1.gcp.mongodb.net/test?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("connected to Saloon APP..."))
+  .catch((err) => console.errr("Could not connect to database...", err));
 //***** ///// *****//
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public/images')));
+app.use(express.static(path.join(__dirname, "public/images")));
 app.set("view engine", "ejs");
-app.use('/public', express.static('public'));
-app.use('/api', indexRouter);
-app.use(function(req, res, next) {
+app.use("/public", express.static("public"));
+app.use("/api", indexRouter);
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
